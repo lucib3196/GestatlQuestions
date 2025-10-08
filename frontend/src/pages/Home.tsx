df8ae4e4a6cdac7571e2777df8933f24e4128d7d
Luciano Bermudez <lucianobmecheng69@gmail.com>
2025-10-08 13:36:11 -0700

    Update this

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -11,6 +11,7 @@ function Hero() {
                 {/* Title */}
                 <h1 className="font-extrabold text-4xl sm:text-6xl text-gray-900 dark:text-white">
                     Gestalt Generator
+                    
                 </h1>
 
                 <hr className="border-primary-yellow dark:border-primary-yellow/70 w-24 mx-auto" />
