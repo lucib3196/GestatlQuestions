fbd70c30a196bcff00d10dd09e33f038ddbd20c2
Luciano Bermudez <lucianobmecheng69@gmail.com>
2025-10-14 17:35:03 -0700

    updated and fixed path issue for test

--- backend/src/app_test/unit/code_runner/conftest.py
+++ backend/src/app_test/unit/code_runner/conftest.py
@@ -8,7 +8,7 @@ class TestConfig(BaseSettings):
 
 
 test_config = TestConfig(
-    asset_path=Path("backend/src/app_test/test_assets/code").resolve()
+    asset_path=Path("src/app_test/test_assets/code").resolve()
 )
 
 
