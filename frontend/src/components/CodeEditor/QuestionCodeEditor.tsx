608b76eb625dd37d9a8d639d6440e3cf68af5b1c
Luciano Bermudez <lucianobmecheng69@gmail.com>
2025-10-17 11:43:21 -0700

    Removed unused value

--- frontend/src/components/CodeEditor/QuestionCodeEditor.tsx
+++ frontend/src/components/CodeEditor/QuestionCodeEditor.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { getFiles } from "../../api";
 import { fetchFileContent, getFileNames, getLanguage } from "../../utils";
 import type { FileData } from "../../types/types";
