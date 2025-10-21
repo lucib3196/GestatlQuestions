201d764b48be90a9778a77e3d9ab62a74cbede2c
Luciano Bermudez <lucianobmecheng69@gmail.com>
2025-10-21 12:28:27 -0700

    Updated the core settings

--- backend/src/ai_base/settings.py
+++ backend/src/ai_base/settings.py
@@ -1,24 +1,53 @@
-import os
-from pydantic_settings import BaseSettings
-from dotenv import load_dotenv
-
-load_dotenv()
-
-
-class AISettings(BaseSettings):
-    PROVIDER: str
-    MODEL: str
-
-    OPENAI_API_KEY: str
-
-    class Config:
-        env_file = ".env"
-        case_sensitive = True
-        extra="ignore" # Ignore extra env stuff
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import BaseModel, Field
+from typing import Optional
+from functools import lru_cache
+from pathlib import Path
+from pydantic import BaseModel, ValidationError
 
 
-ai_settings = AISettings(
-    PROVIDER=str(os.getenv("AI_PROVIDER", "openai")),
-    MODEL=str(os.getenv("MODEL", "gpt-4o")),
-    OPENAI_API_KEY=str(os.getenv("OPENAI_API_KEY")),
-)
+# Points to the root directory C://User/GestaltQuestions adjust as needed
+ROOT_PATH = Path(__file__).parents[3]
+
+target = ROOT_PATH / "ai.env"
+print(target.exists())
+
+
+class AIModel(BaseModel):
+    name: Optional[str] = None  # A name such as (fast, long context etc)
+    provider: str = Field(..., description="e.g. Openai, gemini, claude etc")
+    model: str = Field(..., description="e.g. gpt-4o-mini, claude 3 opus")
+    api_key: Optional[str] = Field(None, description="API Key for this provider")
+    base_url: Optional[str] = None  # optional custom endpoint
+
+
+class AICoreSettings(BaseSettings):
+    default_api_key: Optional[str] = Field(
+        None, description="Fallback key if model missing"
+    )
+    fast_model: Optional[AIModel] = None
+    base_model: AIModel
+    long_context_model: Optional[AIModel] = None
+
+    langsmith_tracing: bool = True
+    langsmith_project: Optional[str] = "ai_ucr"
+
+    def get_api_key(self, model_name: str) -> str | None:
+        model = getattr(self, model_name)
+        return model.api_key or self.default_api_key
+
+    model_config = SettingsConfigDict(
+        env_prefix="AI_", env_file=ROOT_PATH / "ai.env", env_nested_delimiter="__"
+    )
+
+
+@lru_cache
+def get_settings() -> AICoreSettings:
+    return AICoreSettings()  # type: ignore
+
+
+if __name__ == "__main__":
+    try:
+        print(get_settings().model_dump())
+    except ValidationError as e:
+        print(e)
