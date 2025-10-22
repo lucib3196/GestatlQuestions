a33a8209759cf6d4b32d742a05ca07212a8d44bb
Luciano Bermudez <lucianobmecheng69@gmail.com>
2025-10-22 15:27:03 -0700

    Added lecture component

---
+++ frontend/src/pages/LecturePage.tsx
@@ -0,0 +1,9 @@
+import { DerivationRender } from "../components/LectureComponents/Derivation";
+import SectionContainer from "../components/Base/SectionContainer";
+export function LecturePage(){
+    return (
+        <SectionContainer id="derivation" >
+        <DerivationRender/>
+        </SectionContainer>
+    )
+}
