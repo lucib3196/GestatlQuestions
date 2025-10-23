38f838fd56233fa7a107cd9d2c675effb0e79992
Luciano Bermudez <lucianobmecheng69@gmail.com>
2025-10-23 16:06:22 -0700

    Added a basic toast for success

--- frontend/src/components/Auth/SignUpPage.tsx
+++ frontend/src/components/Auth/SignUpPage.tsx
@@ -1,9 +1,11 @@
 import AuthBase from "./AuthBase"
-
+import { toast } from "react-toastify"
 
 export function SignUpPage() {
     const handleSubmit = (name: string, email: string, password: string) => {
         console.log("Submitted", name, email, password)
+        toast.success(`Welcome to Gestalt ${name}`)
+
     }
     return (
         <div className="flex flex-col items-center justify-center">
--- frontend/src/components/Auth/UserLoginPage.tsx
+++ frontend/src/components/Auth/UserLoginPage.tsx
@@ -1,22 +1,18 @@
+import React, { useState } from "react";
 import { MyModal } from "../Base/MyModal";
-import { useState } from "react";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
-import React from "react";
 import { LogInPage } from "./LoginPage";
 import { SignUpPage } from "./SignUpPage";
 import SectionContainer from "../Base/SectionContainer";
 
+import { TitleHeader } from './../Base/TitleHeader';
+
+
+
 type State = "login" | "signup";
 
-function Header({ value }: { value: string }) {
-    return (
-        <div className="flex flex-col items-center text-center space-y-1">
-            <h1 className="text-xl font-semibold tracking-tight">{value}</h1>
-            <div className="w-16 h-[2px] bg-gray-300 rounded" />
-        </div>
-    );
-}
+
 export default function UserLoginPage() {
     const [showModal, setShowModal] = useState<boolean>(true);
     const [state, setState] = React.useState<State>("login");
@@ -47,12 +43,12 @@ export default function UserLoginPage() {
                     </div>
                     {state === "login" ? (
                         <>
-                            <Header value="LogIn"></Header>
+                            <TitleHeader value="LogIn"></TitleHeader>
                             <LogInPage />
                         </>
                     ) : (
                         <>
-                            <Header value="SignUp"></Header>
+                            <TitleHeader value="SignUp"></TitleHeader>
                             <SignUpPage />
                         </>
                     )}
